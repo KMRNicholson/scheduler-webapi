5f94aac43e1deadccf8926c78a5e51a623fe7cce
Skarmac <kohdy.nicholson@gmail.com>
2018-05-07 18:27:53 -0400

    Added a request helper class for return responses to client

---
+++ Controllers/RequestHelper.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using SchedulerWebApi.Models;
+
+namespace SchedulerWebApi.Controllers
+{
+    public partial class RequestHelper
+    {
+        public ActionResult Success(object value)
+        {
+            JsonResult response = new JsonResult(value);
+            response.StatusCode = 200;
+            return response;
+        }
+
+        public ActionResult NotFound()
+        {
+            JsonResult response = new JsonResult(new {});
+            response.StatusCode = 404;
+            return response;
+        }
+
+        public ActionResult Unauthorized()
+        {
+            JsonResult response = new JsonResult(new {});
+            response.StatusCode = 401;
+            return response;
+        }
+
+        public ActionResult BadRequest()
+        {
+            JsonResult response = new JsonResult(new {});
+            response.StatusCode = 500;
+            return response;
+        }
+    }
+    
+}
