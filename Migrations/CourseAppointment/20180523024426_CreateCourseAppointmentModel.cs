2e2a4950810092273c05150af5cfce11d1bd4e0d
Skarmac <kohdy.nicholson@gmail.com>
2018-05-22 22:45:52 -0400

    #17 Added CourseAppointment migration files

---
+++ Migrations/CourseAppointment/20180523024426_CreateCourseAppointmentModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace SchedulerWebApi.Migrations.CourseAppointment
+{
+    public partial class CreateCourseAppointmentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CourseAppointments",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(nullable: false),
+                    AppointmentId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseAppointments", x => new { x.CourseId, x.AppointmentId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseAppointments");
+        }
+    }
+}
